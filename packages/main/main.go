e7df641e5a42570c72d76aa53a2e09b7db5b11c7
Martin <martin.ansong@gmail.com>
2016-01-06 22:51:17 +0000

    Lesson on packages and use of go commands

---
+++ packages/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mansong1/udemyGoLang/packages/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
+	fmt.Println(stringutil.MyName)
+}
